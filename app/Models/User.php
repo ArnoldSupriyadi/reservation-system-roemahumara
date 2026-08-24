53f90435fc99a7d03901c8a902cb519bfb9e8cc0
Arnold Supriyadi <arnold.supriyadi@email.com>
2026-08-24 17:19:26 +0700

    rename return true at user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -90,6 +90,7 @@ class User extends Authenticatable implements FilamentUser
      */
     public function canAccessPanel(Panel $panel): bool
     {
-        return (bool) $this->is_active;
+        // return (bool) $this->is_active;
+        return true;
     }
 }
